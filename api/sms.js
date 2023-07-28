6474f0fbe4738dc46854e7f1665c66b52b24c282
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2023-07-28 17:19:13 -0400

    no wai

--- api/sms.js
+++ api/sms.js
@@ -1,8 +1,8 @@
 const { MessagingResponse } = require('twilio').twiml;
 
 export default function handler(req, res) {
-  console.log(req);
-  if (req.method === 'GET') { return; }
+  if (req.method === 'GET') { return res.send('getter no getting'); }
+  console.log(req.body);
   const twiml = new MessagingResponse();
   twiml.message('The Robots are coming! Head for the hills!');
   res.type('text/xml').send(twiml.toString());
