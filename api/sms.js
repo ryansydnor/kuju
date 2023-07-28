7c42742f8b2c8448b6ec16c7d6a0c647bdbac202
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2023-07-28 18:10:27 -0400

    multiple replies

--- api/sms.js
+++ api/sms.js
@@ -2,9 +2,10 @@ const { MessagingResponse } = require('twilio').twiml;
 
 export default function handler(req, res) {
   if (req.method === 'GET') { return res.send('getter no getting'); }
-  console.log(req.body);
   const twiml = new MessagingResponse();
   twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message('Runn!!');
+  twiml.media('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqHkoeb3TGsE72Ta1r-9C9Q9B42304Q0EYi5iHoAoByw&s');
   res.setHeader('Content-Type', 'text/xml');
   console.log(twiml.toString())
   res.status(200).send(twiml.toString());
