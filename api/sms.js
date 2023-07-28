b3f1155ae44dbba5fe82f4160deac866f5c5fd2b
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2023-07-28 19:33:03 -0400

    hmm

--- api/sms.js
+++ api/sms.js
@@ -5,7 +5,9 @@ module.exports.default = async function handler(req, res) {
   console.log('sms', req.body);
   if (!req.body.Body) { return res.send('need a word or phrase to start with'); }
   const mmsXML = await twilio.generateMMSReply({ body: 'making something just for you' });
-  res.setHeader('Content-Type', 'text/xml').send(mmsXML);
+  res.status(200);
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(mmsXML);
   console.log('fetching poem')
   const fetchRes = await fetch('https://kuju.vercel.app/api/poem', {
     method: 'POST',
@@ -13,4 +15,5 @@ module.exports.default = async function handler(req, res) {
     headers: {'Content-Type': 'application/json'},
   });
   console.log(fetchRes);
+  res.end();
 }
