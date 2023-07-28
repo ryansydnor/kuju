f5071c426a5b32302c5a5138e5c7e53a552ece19
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2023-07-28 18:58:19 -0400

    no img2

--- api/sms.js
+++ api/sms.js
@@ -5,7 +5,7 @@ module.exports.default = async function handler(req, res) {
   if (req.method === 'GET') { return res.send('getter no getting'); }
   console.log(req.body.Body);
   const body = await openai.generatePoem({ prompt: req.body.Body });
-  // const image = await openai.generateImage({ prompt: body });
+  const image = undefined; //await openai.generateImage({ prompt: body });
   const mmsXML = await twilio.generateMMSReply({ body, image });
   res.setHeader('Content-Type', 'text/xml').send(mmsXML);
 }
